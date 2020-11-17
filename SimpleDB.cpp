7e01ce6f6b4eda0eab1f6445777fe6b36aece496
jlegutko@student.agh.edu.pl <jlegutko@student.agh.edu.pl>
2020-11-17 17:32:44 +0100

    Dodaj pliki projektów.

---
+++ SimpleDB.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include<vector>
+#include<stdlib.h>
+
+using std::vector;
+
+
+
+
+class SimpleDB
+{
+public:
+	SimpleDB();
+	~SimpleDB();
+	char Read(FILE) {
+	};
+	vector<char> Write(FILE) {
+	};
+	vector<char> Delete(FILE, int field_num) {
+	};
+private:
+
+};
+
+SimpleDB::SimpleDB()
+{
+}
+
+SimpleDB::~SimpleDB()
+{
+}
